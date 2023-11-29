94735aca653a10cb32fe80186b34e3e906a2a707
eunne <85356011+eunne@users.noreply.github.com>
2023-11-30 02:24:14 +0900

    1st trial_raw code

---
+++ main.py
@@ -0,0 +1,14 @@
+def get_yearly_revenue(monthly_revenue):
+  revenue_for_a_year = return monthly_revenue * 12
+
+def get_yearly_expenses(monthly_expense):
+  expense_for_a_year = return monthly_expense * 12
+
+def get_tax_amount(profit):
+  if profit >100000:
+    tax_amount = return profit * 25%
+  else:
+    tax_amount = return profit * 15%
+
+def apply_tax_credits(tax_amount, tax_credits):
+  return tax_amount * tax_credits
