9d0172d9f2c42cdcbdf26f71c6e545c1c00d04e2
eunne <85356011+eunne@users.noreply.github.com>
2023-11-30 03:02:46 +0900

    change order of var, simplify vars

--- main.py
+++ main.py
@@ -14,14 +14,12 @@ def apply_tax_credits(tax_amount, tax_credits):
   return tax_amount * tax_credits
 
 
-revenue_for_a_year = get_yearly_revenue(monthly_revenue)
-expense_for_a_year = get_yearly_expenses(monthly_expenses)
-
 monthly_revenue = 5500000
 monthly_expenses = 2700000
 tax_credits = 0.01
 
-profit = revenue_for_a_year - expense_for_a_year
+#no need to input every function in variable
+profit = get_yearly_revenue(monthly_revenue) - get_yearly_expenses(monthly_expenses)
 
 tax_amount = get_tax_amount(profit)
 
