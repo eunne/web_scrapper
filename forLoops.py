1be331a4c73098c13cdf5529b4eb086b41e27475
eunne <85356011+eunne@users.noreply.github.com>
2023-12-03 07:23:34 +0900

    use requests library

--- forLoops.py
+++ forLoops.py
@@ -1,14 +1,22 @@
-import requests 
+from requests import get
 
-websites = [
+websites = (
     "google.com",
     "https://facebook.com",
     "apple.com",
     "https://tiktok.com",
     "microsoft.com"
-  ]
+  )
+
+results = {}
 
 for website in websites:
   if not website.startswith("https://"):
     website = f"https://{website}"
-  print(website)
+  response = get(website)
+  if response.status_code == 200:
+    results[website] = "working"ㅔㅑ
+  else:
+    results[website] = "not working"
+
+print(results)
