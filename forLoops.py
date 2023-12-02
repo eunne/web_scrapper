f5e1653c521f7c97073feeeb2bc755fac2a136b2
eunne <85356011+eunne@users.noreply.github.com>
2023-12-03 08:03:08 +0900

    scrapper_get request

--- forLoops.py
+++ forLoops.py
@@ -1,5 +1,5 @@
 from requests import get
-
+'''
 websites = (
     "google.com",
     "https://facebook.com",
@@ -15,8 +15,19 @@ for website in websites:
     website = f"https://{website}"
   response = get(website)
   if response.status_code == 200:
-    results[website] = "working"ㅔㅑ
+    results[website] = "working"
   else:
     results[website] = "not working"
 
 print(results)
+'''
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python" #input("input a job to seartch:")
+
+response = get(f"{base_url}{search_term}")
+
+if response.status_code != 200:
+  print("can't response website")
+else:
+  print(response.text)
