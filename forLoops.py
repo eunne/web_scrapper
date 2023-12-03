a3b3e8139a3837437de9b57f1bcea218421a8820
eunne <85356011+eunne@users.noreply.github.com>
2023-12-03 18:57:01 +0900

    grap job post using bs4,request

--- forLoops.py
+++ forLoops.py
@@ -1,4 +1,5 @@
 from requests import get
+from bs4 import BeautifulSoup
 '''
 websites = (
     "google.com",
@@ -23,11 +24,22 @@ print(results)
 '''
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term = "python" #input("input a job to seartch:")
+search_term = "react" #input("input a job to seartch:")
 
 response = get(f"{base_url}{search_term}")
 
 if response.status_code != 200:
   print("can't response website")
 else:
-  print(response.text)
+  soup = BeautifulSoup(response.text, "html.parser")
+  #section -> class=jobs 정보를 모두 가져옴
+  job_sections = soup.find_all('section', class_="jobs")
+  
+  #job_section 중에서 li인 항목만 가져옴
+  for job_section in job_sections:
+    job_posts = job_section.find_all('li')
+
+    #job_post중에서 필요없는 정보제거(view_all)
+    job_posts.pop(-1)
+    for job_post in job_posts:
+      print(job_post)
