93dd9ef3ece0579cadb28690822e00716713108e
eunne <85356011+eunne@users.noreply.github.com>
2023-12-08 07:15:39 +0900

    make job seraching website server using flask

---
+++ exmaple/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#user가 방문하면 아래 def 함수를 호출함.
+@app.route("/") 
+#decorator, 아래 def와 꼭 함께 있어야함. 그래야 작동.
+#이렇게 def랑 떨어져있으면 작동안함!!
+def home():
+  return render_template("home.html", name="Eunne")
+
+@app.route("/search")
+def search():
+  return render_template("search.html")
+
+#server 만듦. user의 request받을 수 있음
+app.run("127.0.0.1", debug=True)
